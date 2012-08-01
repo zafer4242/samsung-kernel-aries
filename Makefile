c3d93407469ae7d73264deef43713da8952a3cc8
Kasper Hettinga <kasper.hettinga@gmail.com>
2012-08-01 16:56:51 +0100

    Update to the July build of Linaro
    Change-Id: I5328b9b99bb37d38cdd0d193415a695cb67c50fa

--- Makefile
+++ Makefile
@@ -193,7 +193,8 @@ SUBARCH := $(shell uname -m | sed -e s/i.86/i386/ -e s/sun4u/sparc64/ \
 # Note: Some architectures assign CROSS_COMPILE in their arch/*/Makefile
 export KBUILD_BUILDHOST := $(SUBARCH)
 ARCH		?= arm
-CROSS_COMPILE	?= /home/kasper/android/system/prebuilt/linux-x86/toolchain/gcc-linaro-arm-linux-gnueabihf-2012.06-20120625_linux/bin/arm-linux-gnueabihf-
+CROSS_COMPILE	?= /home/kasper/android/system/prebuilt/linux-x86/toolchain/gcc-linaro-arm-linux-gnueabihf-2012.07-20120720_linux/bin/arm-linux-gnueabihf-
+#CROSS_COMPILE	?= /home/kasper/android/system/prebuilt/linux-x86/toolchain/gcc-linaro-arm-linux-gnueabihf-2012.06-20120625_linux/bin/arm-linux-gnueabihf-
 
 # Architecture as present in compile.h
 UTS_MACHINE 	:= $(ARCH)
--- sbuild.sh
+++ sbuild.sh
@@ -7,15 +7,18 @@ kernel_ver="Mackay_kernel_0.34"
 export LOCALVERSION="-"`echo $kernel_ver`
 export ANDROID_BUILD_TOP=/home/kasper/android/system
 
-make CROSS_COMPILE=$ANDROID_BUILD_TOP/prebuilt/linux-x86/toolchain/gcc-linaro-arm-linux-gnueabihf-2012.06-20120625_linux/bin/arm-linux-gnueabihf- ARCH=arm aries_galaxysmtd_defconfig
+make CROSS_COMPILE=$ANDROID_BUILD_TOP/prebuilt/linux-x86/toolchain/gcc-linaro-arm-linux-gnueabihf-2012.07-20120720_linux/bin/arm-linux-gnueabihf- ARCH=arm aries_galaxysmtd_defconfig
+## make CROSS_COMPILE=$ANDROID_BUILD_TOP/prebuilt/linux-x86/toolchain/gcc-linaro-arm-linux-gnueabihf-2012.06-20120625_linux/bin/arm-linux-gnueabihf- ARCH=arm aries_galaxysmtd_defconfig
 ## make CROSS_COMPILE=$ANDROID_BUILD_TOP/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi- ARCH=arm aries_galaxysmtd_defconfig
 
-make CROSS_COMPILE=$ANDROID_BUILD_TOP/prebuilt/linux-x86/toolchain/gcc-linaro-arm-linux-gnueabihf-2012.06-20120625_linux/bin/arm-linux-gnueabihf- ARCH=arm -j8 modules
+make CROSS_COMPILE=$ANDROID_BUILD_TOP/prebuilt/linux-x86/toolchain/gcc-linaro-arm-linux-gnueabihf-2012.07-20120720_linux/bin/arm-linux-gnueabihf- ARCH=arm -j8 modules
+## make CROSS_COMPILE=$ANDROID_BUILD_TOP/prebuilt/linux-x86/toolchain/gcc-linaro-arm-linux-gnueabihf-2012.06-20120625_linux/bin/arm-linux-gnueabihf- ARCH=arm -j8 modules
 ## make CROSS_COMPILE=$ANDROID_BUILD_TOP/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi- ARCH=arm -j8 modules
 
 find $ANDROID_BUILD_TOP/kernel/samsung/aries -name '*.ko' -exec cp -v {} $ANDROID_BUILD_TOP/kernel/samsung/ics-ramdisk/ics_combo/files/modules \;
 
-make CROSS_COMPILE=$ANDROID_BUILD_TOP/prebuilt/linux-x86/toolchain/gcc-linaro-arm-linux-gnueabihf-2012.06-20120625_linux/bin/arm-linux-gnueabihf- ARCH=arm -j8 zImage
+make CROSS_COMPILE=$ANDROID_BUILD_TOP/prebuilt/linux-x86/toolchain/gcc-linaro-arm-linux-gnueabihf-2012.07-20120720_linux/bin/arm-linux-gnueabihf- ARCH=arm -j8 zImage
+## make CROSS_COMPILE=$ANDROID_BUILD_TOP/prebuilt/linux-x86/toolchain/gcc-linaro-arm-linux-gnueabihf-2012.06-20120625_linux/bin/arm-linux-gnueabihf- ARCH=arm -j8 zImage
 ## make CROSS_COMPILE=$ANDROID_BUILD_TOP/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi- ARCH=arm -j8 zImage
 
 cd arch/arm/boot
