f74350a916590c2095c6261fe0b2b1cc6373a507
Kasper Hettinga <kasper.hettinga@gmail.com>
2012-11-22 20:02:45 +0100

    Add 1400MHz step
    Change-Id: Ia610ab72cb37073357bc58189560691559d65410

--- arch/arm/mach-s5pv210/mach-aries.c
+++ arch/arm/mach-s5pv210/mach-aries.c
@@ -410,7 +410,11 @@ static struct s5p_media_device aries_media_devs[] = {
 #ifdef CONFIG_CPU_FREQ
 static struct s5pv210_cpufreq_voltage smdkc110_cpufreq_volt[] = {
 	{
-		.freq	= 1200000,
+		.freq	= 1400000,
+		.varm	= 1350000,
+		.vint	= 1150000,
+	}, {
+		.freq	= 1000000,
 		.varm	= 1275000,
 		.vint	= 1100000,
 	}, {
